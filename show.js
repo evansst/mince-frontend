1be430e8d94fa0d1a6acc8e5d31f78bb5cd4c8b8
NyaradzoUBere <nybe5598@colorado.edu>
2020-07-28 15:04:47 -0600

    class and id names

--- show.js
+++ show.js
@@ -4,18 +4,11 @@ const id = searchParams.get('id')
 fetch(`http://localhost:3000/recipes/${id}`)
     .then(response => response.json())
     .then(showRecipe)
-    // .then(recipe => {
-    //     const h1 = document.createElement('h1')
-    //     h1.innerText = recipe.name
-    //     document.body.appendChild(h1)
-    // })
 
 function showRecipe(recipe) {
-    // recipes.forEach(recipe => {
         const h1 = document.createElement('h1')
         h1.innerText = recipe.name
         document.body.appendChild(h1)
-
         const p = document.createElement('p')
         p.innerHTML = `<a href ='${recipe.url}'>${recipe.name}</a>`
         document.body.appendChild(p)
