6f02251fa7ef05b82f72b4e0df3082aa4363fb01
Sam Evans <evansst@icloud.com>
2020-07-29 13:19:13 -0600

    adds login and logout forms to homepage

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const user_id = searchParams.get('user_id');
 
 const baseURL = "http://localhost:3000";
 let recipeURL = `${baseURL}/recipes`;
+let userURL = `${baseURL}/users/`;
 
+
+
+
+// Logic so we can still have the back-end controller return all recipes if we want
 
 if (searchName) {
   recipeURL = `${recipeURL}?name=${searchName}`;
@@ -13,13 +18,26 @@ if (searchName) {
   recipeURL = `${recipeURL}?sample=9`;
 }
 
+
+const $section1 = document.querySelector('.section-1');
+const $section2 = document.querySelector('.section-2');
 const $section3 = document.querySelector('.section-3');
+
+if (user_id) {
+  userURL = `${userURL}/${user_id}`;
+  fetch(userURL)
+    .then(parseJSON)
+    .then(displayLogOut);
+} else {
+  fetch(userURL)
+    .then(parseJSON)
+    .then(displayLogIn);
+}
 
 
 fetch(recipeURL)
     .then(parseJSON)
-    .then(displayPage)
-    .then(console.log);
+    .then(displayPage);
   
 
 function parseJSON(response) {
@@ -36,6 +54,8 @@ function displayPage(recipes) {
   }
   return recipes;
 }
+
+// Render filtered or sampled recipes
 
     
 function recipeToElement(recipe) {
@@ -64,8 +84,55 @@ function showRecipes($recipeCard) {
   $section3.append($recipeCard);
 }
 
+// What is this?
+
 window.scroll({
   top: 100,
   left: 100,
   behavior: 'smooth'
 });
+
+// User controls in header
+
+function displayLogIn(users) {
+  const $form = document.createElement('form');
+  $form.innerHTML = `
+    <form class='login_form' id='login'>
+      <label for='login_input'>Username:</label>
+    </form>`;
+
+  const $select = document.createElement('select');
+  $select.name = 'user_id';
+  const $submit = document.createElement('input');
+
+  $submit.type = 'submit';
+  $submit.value = 'Submit';
+
+  $form.append(addUserOptions($select, users), $submit);
+    
+  $section1.append($form);
+  return users;
+}
+
+function addUserOptions($select, users) {
+  $userOptions = users.map(userToOption);
+  $userOptions.forEach($userOption => $select.append($userOption));
+
+  return $select;
+}
+
+function userToOption(user) {
+  const $option = document.createElement('option');
+  $option.innerText = user.user_name;
+  $option.value = user.id;
+  return $option;
+}
+
+function displayLogOut(user) {
+  const $p = document.createElement('p');
+  $p.innerHTML = `Logged in as <a href=user.html?user_id=${user.id}>${user.user_name}</a> <br> <a href='index.html'>Log Out</a>`;
+
+  $section1.append($p);
+
+  return user;
+}
